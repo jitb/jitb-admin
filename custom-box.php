cf54d3a779769f270946f867592219f9114b0198
System Administrator <root@iMac-3.local>
2017-08-31 09:23:12 +0800

    Update flamingo user permissions function

--- custom-box.php
+++ custom-box.php
@@ -238,22 +238,31 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 
 function jitb_flamingo_map_meta_cap( $meta_caps ) {
   
-  $meta_caps = array_merge( $meta_caps, array(
-    'flamingo_edit_contact' => 'publish_posts',
-    'flamingo_edit_contacts' => 'publish_posts',
-    'flamingo_delete_contact' => 'publish_posts',
+  $meta_caps = array(
+  
+    'flamingo_edit_contact' => 'edit_posts',
+    'flamingo_edit_contacts' => 'edit_posts',
+    'flamingo_delete_contact' => 'edit_posts',
     'flamingo_edit_inbound_message' => 'publish_posts',
     'flamingo_edit_inbound_messages' => 'publish_posts',
     'flamingo_delete_inbound_message' => 'publish_posts',
     'flamingo_delete_inbound_messages' => 'publish_posts',
     'flamingo_spam_inbound_message' => 'publish_posts',
     'flamingo_unspam_inbound_message' => 'publish_posts',
-  ) );
+    'flamingo_edit_outbound_message' => 'publish_posts',
+    'flamingo_edit_outbound_messages' => 'publish_posts',
+    'flamingo_delete_outbound_message' => 'publish_posts',
+  );
 
-  return $meta_caps;
+  $caps = array_diff( $caps, array_keys( $meta_caps ) );
+
+  if ( isset( $meta_caps[$cap] ) )
+      $caps[] = $meta_caps[$cap];
+
+  return $caps;
 }
-
-add_filter( 'flamingo_map_meta_cap', __NAMESPACE__ . '\\jitb_flamingo_map_meta_cap' );
+remove_filter( 'map_meta_cap', 'flamingo_map_meta_cap' );
+add_filter( 'map_meta_cap', __NAMESPACE__ . '\\jitb_flamingo_map_meta_cap', 9, 4 );
 
 
 
