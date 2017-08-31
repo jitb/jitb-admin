ae4175a589f0ddfb2b64ed9fb82b989cbc5173dc
System Administrator <root@iMac-3.local>
2017-08-31 10:20:48 +0800

    Update flamingo user permissions function. Update version: 2.0.3

--- custom-box.php
+++ custom-box.php
@@ -236,9 +236,12 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 // Change Permissions: Flamingo viewable as Editor
 // --------------------------------------------------
 
-function jitb_flamingo_map_meta_cap( $meta_caps ) {
+remove_filter( 'map_meta_cap', 'flamingo_map_meta_cap' );
+add_filter( 'map_meta_cap', __NAMESPACE__ . '\\jitb_flamingo_map_meta_cap', 9, 4 );
+
+function jitb_flamingo_map_meta_cap( $caps, $cap, $user_id, $args ) {
+    $meta_caps = array(
   
-  $meta_caps = array_merge( $meta_caps, array(
     'flamingo_edit_contact' => 'edit_posts',
     'flamingo_edit_contacts' => 'edit_posts',
     'flamingo_delete_contact' => 'edit_posts',
@@ -251,12 +254,15 @@ function jitb_flamingo_map_meta_cap( $meta_caps ) {
     'flamingo_edit_outbound_message' => 'publish_posts',
     'flamingo_edit_outbound_messages' => 'publish_posts',
     'flamingo_delete_outbound_message' => 'publish_posts',
-  ) );
+  );
 
-  return $meta_caps;
+  $caps = array_diff( $caps, array_keys( $meta_caps ) );
+
+  if ( isset( $meta_caps[$cap] ) )
+      $caps[] = $meta_caps[$cap];
+
+  return $caps;
 }
-
-add_filter( 'flamingo_map_meta_cap', __NAMESPACE__ . '\\jitb_flamingo_map_meta_cap' );
 
 
 // Change Admin Menu Labels
